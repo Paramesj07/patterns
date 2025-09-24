4df38d5221c8cc7727d5ea98e6577e19f9daecd3
Paramesj07 <parameswaranj07@gmail.com>
2025-09-24 23:50:22 +0530

    modify

--- pat1.java
+++ pat1.java
@@ -4,7 +4,6 @@ class pat1
     public static void main(String[]args)
     {
         int n=5;
-        int k=0;
         for(int i=0;i<n;i++){
             for(int j=0; j<n; j++){
                 if(i+j<=n-1 && i>=j ||i+j>=n-1 && i<=j){
